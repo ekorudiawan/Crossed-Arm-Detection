a21ea8f4ca2fa71d170ea1920ea0aeb39bce90aa
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2019-04-21 15:03:19 +0800

    Sudah pakai generalized hough transform

--- sources/cross-arm.py
+++ sources/cross-arm.py
@@ -2,12 +2,16 @@ import cv2 as cv
 import numpy as np 
 import os 
 import math
+from collections import defaultdict
 
 click_count = 0
 top_x = 0
 top_y = 0
 bot_x = 0
 bot_y = 0
+
+MIN_CANNY_THRESHOLD = 0
+MAX_CANNY_THRESHOLD = 255
 
 def click_event(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
@@ -22,53 +26,111 @@ def click_event(event, x, y, flags, param):
             bot_y = y
             print("Second ROI", bot_x, bot_y)
             click_count = 0
+
+def gradient_orientation(image):
+    '''
+    Calculate the gradient orientation for edge point in the image
+    '''
+    # dx = sobel(image, axis=0, mode='constant')
+    # dy = sobel(image, axis=1, mode='constant')
+    dx = cv.Sobel(image, cv.CV_64F, 1,0, ksize=5)
+    dy = cv.Sobel(image, cv.CV_64F, 0,1, ksize=5)
+    gradient = np.arctan2(dy,dx) * 180 / np.pi
+
+    return gradient
+
+def build_r_table(image, origin):
+    '''
+    Build the R-table from the given shape image and a reference point
+    '''
+    edges = cv.Canny(image, MIN_CANNY_THRESHOLD, MAX_CANNY_THRESHOLD) 
+    gradient = gradient_orientation(edges)
     
+    r_table = defaultdict(list)
+    for (i,j),value in np.ndenumerate(edges):
+        if value:
+            r_table[gradient[i,j]].append((origin[0]-i, origin[1]-j))
+
+    return r_table
+
+def accumulate_gradients(r_table, grayImage):
+    '''
+    Perform a General Hough Transform with the given image and R-table
+    '''
+    edges = cv.Canny(grayImage, MIN_CANNY_THRESHOLD, MAX_CANNY_THRESHOLD)
+    gradient = gradient_orientation(edges)
+    
+    accumulator = np.zeros(grayImage.shape)
+    # print("acc shape", accumulator.shape)
+    for (i,j),value in np.ndenumerate(edges):
+        if value:
+            for r in r_table[gradient[i,j]]:
+                accum_i, accum_j = i+r[0], j+r[1]
+                
+                if accum_i >= 0 and accum_i < accumulator.shape[0] and accum_j >= 0 and accum_j < accumulator.shape[1]:
+                    # print("accum ", accum_i, accum_j)
+                    accumulator[int(accum_i), int(accum_j)] += 1
+                    
+    return accumulator
+
+def general_hough_closure(reference_image):
+    '''
+    Generator function to create a closure with the reference image and origin
+    at the center of the reference image
+    
+    Returns a function f, which takes a query image and returns the accumulator
+    '''
+    referencePoint = (reference_image.shape[0]/2, reference_image.shape[1]/2)
+    r_table = build_r_table(reference_image, referencePoint)
+    
+    def f(query_image):
+        return accumulate_gradients(r_table, query_image)
+        
+    return f
+
+def n_max(a, n):
+    '''
+    Return the N max elements and indices in a
+    '''
+    indices = a.ravel().argsort()[-n:]
+    indices = (np.unravel_index(i, a.shape) for i in indices)
+    return [(a[i], i) for i in indices]
+
+def test_general_hough(gh, reference_image, query_image):
+    accumulator = gh(query_image)
+    m = n_max(accumulator, 5)
+    print("m", m)
+    max_prob = [prob[0] for prob in m]
+    y_points = [pt[1][0] for pt in m]
+    x_points = [pt[1][1] for pt in m] 
+    i, j = np.unravel_index(accumulator.argmax(), accumulator.shape)   
+    print("max ", max_prob)
+    return x_points, y_points
+
 def main():
     print("Arm Detection")
     print(cv.__version__)
-    # alg = cv.GeneralizedHough()
-    # print(algo.read())
-    # ballard = cv.GeneralizedHoughBallard()
     img_number = 53
     img_max = 59
     img_min = 53
     template = np.zeros((5,5))
-    # alg.setTemplate(template)
     start_detect = False
+    detect_s = None
     while True:
         filename = '\\images\\images'+str(img_number)+'.png'
         file_location = os.getcwd() + filename
-        # print("File Location : ", file_location)
         rgb_img = cv.imread(file_location)
         gray_img = cv.cvtColor(rgb_img, cv.COLOR_BGR2GRAY)
         blur_img = cv.GaussianBlur(gray_img, (5, 5), 0)
-        # sobelx = cv.Sobel(blur_img, cv.CV_64F, 1, 0, ksize=5)
-        # sobely = cv.Sobel(blur_img, cv.CV_64F, 0, 1, ksize=5)
-        # sobel = sobelx + sobely
-        # laplacian = cv.Laplacian(blur_img, cv.CV_64F)
-        canny = cv.Canny(blur_img, 0, 150)
-        # print("canny shape", rgb_img.shape)
-        # cv.imshow('RGB Image', rgb_img)
-        # lines = cv.HoughLines(canny, 1, np.pi / 90, 150, None, 0, 0)
-    
-        # if lines is not None:
-        #     for i in range(0, len(lines)):
-        #         rho = lines[i][0][0]
-        #         theta = lines[i][0][1]
-        #         a = math.cos(theta)
-        #         b = math.sin(theta)
-        #         x0 = a * rho
-        #         y0 = b * rho
-        #         pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-        #         pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        #         cv.line(rgb_img, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+        canny = cv.Canny(blur_img, MIN_CANNY_THRESHOLD, MAX_CANNY_THRESHOLD)
+
         if start_detect:
-            # print(type(alg))
-            position, votes = alg.detect(canny)
-            # print("Result ", alg.read())
+            x_points, y_points = test_general_hough(detect_s, template, canny)
+            for i in range(len(x_points)):
+                rgb_img = cv.circle(rgb_img,(x_points[i],y_points[i]), 50, (0,0,255), 1)
             
-        # cv.imshow('RGB Image', rgb_img)
         cv.imshow('Gray Image', canny)
+        cv.imshow('RGB Image', rgb_img)
         cv.imshow('Template', template)
         cv.setMouseCallback('Gray Image', click_event)
         key = cv.waitKey(1)
@@ -85,14 +147,11 @@ def main():
         # Save template
         elif key == ord('t'):
             template = canny[top_y:bot_y,top_x:bot_x]
-            row, col = template.shape
-            cx = col // 2
-            cy = row // 2
-            print("Center ", cx, cy)
-            gh = cv.GeneralizedHough()
-            print(type(gh))
-            gh.setTemplate(template)
-            # alg.setTemplate(template)
+            # row, col = template.shape
+            # cx = col // 2
+            # cy = row // 2
+            # print("Center ", cx, cy)
+            detect_s = general_hough_closure(template)
         # Start matching
         elif key == ord('d'):
             start_detect = True
